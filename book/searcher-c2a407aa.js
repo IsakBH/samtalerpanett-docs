f483f9123b347373c960c2117278f2b3f3db4663
isakbh <isak@brunhenriksen.net>
2026-02-11 17:41:35 +0100

    ok

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-20a02e26.js',
+                path_to_root + 'searchindex-1a8b5e39.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- src/websocketerrorexamplelog.md
+++ src/websocketerrorexamplelog.md
@@ -1,2 +1,3 @@
 # websocket_error.example.log
 Denne eksempel filen er tom. Filen fylles inn dynamisk av webserveren når den får en error.
+Den sånn lowkey printer ikke ut shit da, men det går fint
